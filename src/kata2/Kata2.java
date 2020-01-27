41d00049d3ddd72a68cc2d08a0a44ee1e7c80286
RubenGarciaQuintana <ruben.garcia110@alu.ulpgc.es>
2020-01-27 15:51:33 +0100

    Kata 1 version 3 usando LocalDate y Period.

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -1,21 +1,28 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package kata2;
+/*
 
-/**
- *
- * @author senyo
- */
+@author Ruben Garcia Quintana
+
+*/
+import java.util.*;
+
 public class Kata2 {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) { 
+        int [] data = {1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9};
+        //Histogram histo = new Histogram(data);
+        HashMap<Integer,Integer> histogram = new HashMap<Integer, Integer>();
+        
+        for (int i = 0; i < data.length; i++) {
+            if(histogram.containsKey(data[i])){
+                histogram.put(data[i],histogram.get(data[i])+1);
+            }else{
+                histogram.put(data[i], 1);
+            }
+            
+        }
+            for (Map.Entry<Integer, Integer> entry : histogram.entrySet()) {
+            System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
+            } 
     }
-    
 }
