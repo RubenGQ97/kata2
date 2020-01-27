551dc3653d5ac006b4d26dad3a4687c78ce7125f
RubenGarciaQuintana <ruben.garcia110@alu.ulpgc.es>
2020-01-27 16:02:17 +0100

    Kata 2 version 2

--- src/kata2/Kata2.java
+++ src/kata2/Kata2.java
@@ -13,12 +13,9 @@ public class Kata2 {
         //Histogram histo = new Histogram(data);
         HashMap<Integer,Integer> histogram = new HashMap<Integer, Integer>();
         
-        for (int i = 0; i < data.length; i++) {
-            if(histogram.containsKey(data[i])){
-                histogram.put(data[i],histogram.get(data[i])+1);
-            }else{
-                histogram.put(data[i], 1);
-            }
+        for (int key : data) {
+            histogram.put(key, histogram.containsKey(key) ? histogram.get(key) + 1 : 1);
+
             
         }
             for (Map.Entry<Integer, Integer> entry : histogram.entrySet()) {
